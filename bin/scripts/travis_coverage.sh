a5c8c02a4e6bcdd380e1bd70228ace9d3868a0a4
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2015-08-14 20:43:58 +0200

    simplify this buildscript, introduce "new" bash result fetch handler, change script call sequence ...

--- bin/scripts/travis_coverage.sh
+++ bin/scripts/travis_coverage.sh
@@ -3,8 +3,13 @@
 # travis call for phpunit code coverage generation
 #
 
-echo "== sys::_travis, execute phpUnit codeCoverage ...";
-cd ../../ && phpunit --coverage-clover='coverage.clover.xml' --configuration ./tests/phpunit.xml
-cd ./bin/scripts
+cd ../.. && phpunit --coverage-clover='coverage.clover.xml' --configuration ./tests/phpunit.xml && cd ./bin/scripts
+
+if [ $? -eq 0 ]
+then
+  echo "\nPHPUnit coverage run succeeded"
+else
+  echo "\nPHPUnit coverage run failed" >&2
+fi
 
 exit 0;
