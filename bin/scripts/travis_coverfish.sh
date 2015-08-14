d5757122e2897a494707a05f25f539475a8140fb
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2015-08-14 20:59:28 +0200

    change new (unknown) options from current coverfish self check travis script

--- bin/scripts/travis_coverfish.sh
+++ bin/scripts/travis_coverfish.sh
@@ -3,7 +3,7 @@
 # travis call for phpunit code coverage generation
 #
 
-php ../../bin/coverfish scan --raw-scan-path ../../tests/ --raw-autoload-file ../../vendor/autoload.php --raw-exclude-path tests/data --output-level 1 --no-ansi
+php ../../bin/coverfish scan --scan-path ../../tests/ --autoload-file ../../vendor/autoload.php --exclude-path tests/data --output-level 1 --no-ansi
 
 if [ $? -eq 0 ]
 then
