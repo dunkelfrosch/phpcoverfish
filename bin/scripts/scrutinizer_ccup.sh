33902022a41f64e6395e9c4da407cde5b637905e
Patrick Paechnatz <patrick.paechnatz@gmail.com>
2015-08-14 20:42:41 +0200

    simplify this buildscript, remove obsolete echo lines and exit code

--- bin/scripts/scrutinizer_ccup.sh
+++ bin/scripts/scrutinizer_ccup.sh
@@ -3,10 +3,5 @@
 # scrutinizier code coverage upload
 #
 
-echo "== sys::_scrutinizer, setup ocular.phar archive";
 wget -q -N https://scrutinizer-ci.com/ocular.phar ocular.phar
-echo "== sys::_scrutinizer, upload code coverage result ...";
 php ocular.phar code-coverage:upload --format=php-clover "../../coverage.clover.xml"
-echo "== sys::_scrutinizer, all jobs done";
-
-exit 0;
